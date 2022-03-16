cd619736fd31c1a4d2457055b3c046c78aae4a1f
Anahid <anahidhemmat2130@gmail.com>
2022-03-16 20:23:21 +0400

    done

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function displayTime() {
   }
   document.getElementById("hours").textContent = hrs;
   document.getElementById("minutes").textContent = min;
+  // use query selector
   document.querySelector(".seconds").textContent = sec;
 }
 setInterval(displayTime, 10);
