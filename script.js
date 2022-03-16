9feabc0c659926b2d0bb6f44a97d25f5ecd82e1e
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-03-16 19:02:58 +0330

    complete

---
+++ script.js
@@ -0,0 +1,17 @@
+function displayTime() {
+  let dateTime = new Date();
+  let hrs = dateTime.getHours();
+  let min = dateTime.getMinutes();
+  let sec = dateTime.getSeconds();
+  let session = document.getElementById("session");
+
+  if (hrs >= 12) {
+    session.textContent = "PM";
+  } else {
+    session.textContent = "AM";
+  }
+  document.getElementById("hours").textContent = hrs;
+  document.getElementById("minutes").textContent = min;
+  document.getElementById("seconds").textContent = sec;
+}
+setInterval(displayTime, 10);
