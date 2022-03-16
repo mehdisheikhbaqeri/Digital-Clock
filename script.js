3e674e9fa2a25e36bfc3caf542a72a5686616d56
Anahid <anahidhemmat2130@gmail.com>
2022-03-16 20:22:30 +0400

    Donee

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ function displayTime() {
   }
   document.getElementById("hours").textContent = hrs;
   document.getElementById("minutes").textContent = min;
-  document.getElementById("seconds").textContent = sec;
+  document.querySelector(".seconds").textContent = sec;
 }
 setInterval(displayTime, 10);
